77726bf034d39c44bc5b51b617b32b38a3debbcb
Showmik Mondal <showmik426@gmail.com>
2026-04-10 16:02:43 +0600

    some practice

---
+++ nested.js
@@ -0,0 +1,20 @@
+const money = 0;
+if(money > 300){
+    console.log('bro you are rich!!!');
+
+}
+
+else{
+    if(money > 100){
+        console.log('tui gorib o na boro lok na')
+    }
+    else{
+        if (money > 0){
+            console.log('dosto kola kha, Calcium bara')
+
+        }
+        else {
+            console.log('tui amar bondhu na.')
+        }
+    }
+}
