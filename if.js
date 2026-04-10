68d37c7a7fc102ec8a7fb5c4a61d6cd2723f48cb
Showmik Mondal <showmik426@gmail.com>
2026-04-10 13:20:01 +0600

    learning if else

---
+++ if.js
@@ -0,0 +1,21 @@
+if(13 < 10){
+    console.log('i have a smaller value')
+
+    
+}
+
+var briyaniPrice = 250;
+
+if(briyaniPrice < 300){
+    console.log('mama give me some briyani')
+}
+
+var weight = 20;
+
+if(weight <= 20) {
+    console.log('i will carry this')
+}
+
+else{
+    console.log('i will rent a rickshaw')
+}
