c8554849c91911ec0b4211ba1abcc6084eb4a2f5
Showmik Mondal <showmik426@gmail.com>
2026-04-10 15:21:45 +0600

    practice multiple condition

---
+++ multiple.js
@@ -0,0 +1,24 @@
+const salary = 26000;
+const isBCS = true;
+const height = 68; 
+const hasCar = false;
+
+// if(salary > 20000 && height > 66 ){
+//     console.log('su--patro')
+// }
+
+// else{
+//     console.log('onno--patro--khujiii')
+// }
+
+// if(salary > 25000 || height > 72){
+//     console.log('eso baba kobul ');
+// }
+
+// else{
+//     console.log('vag tui mokbul')
+// }
+
+if ((salary > 25000 && hasCar == true) || isBCS == true){
+    console.log('choddo-gusti-raji')
+}
